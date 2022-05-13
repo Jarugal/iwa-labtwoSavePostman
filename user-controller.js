b91583d80e2b3e8794b98824ee2a8dfeb39f4a70
Jarugal <jargal620@gmail.com>
2022-05-13 20:30:54 +0000

    routes.js user-controller.js

--- user-controller.js
+++ user-controller.js
@@ -7,12 +7,13 @@ exports.createUser = function(req, res){
             res.status(400).json(err);
         }
         res.json(user);
-    });
+    })
 };
 exports.getUsers = function(req, res){
     User.find({}, function(err, users){
         if(err){
             res.status(400).json(err);
         }
-    });
+        res.json(users);
+    })
 };
